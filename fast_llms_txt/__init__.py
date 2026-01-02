170ce101f23ef561311fa85149418a627b52540b
Sam Keen <sam.sjk@gmail.com>
2026-01-02 15:38:51 -0800

    Bump version to 0.4.0

--- fast_llms_txt/__init__.py
+++ fast_llms_txt/__init__.py
@@ -4,4 +4,4 @@ from .generator import generate_llms_txt, get_operation_spec
 from .router import create_llms_txt_router
 
 __all__ = ["create_llms_txt_router", "generate_llms_txt", "get_operation_spec"]
-__version__ = "0.3.0"
+__version__ = "0.4.0"
