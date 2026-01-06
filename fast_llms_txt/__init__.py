97702251f4edde375e70ee757d4475b7209b282a
Sam Keen <sam.sjk@gmail.com>
2026-01-06 09:56:05 -0800

    Bump version to 0.5.5

--- fast_llms_txt/__init__.py
+++ fast_llms_txt/__init__.py
@@ -4,4 +4,4 @@ from .generator import generate_llms_txt
 from .router import create_llms_txt_router
 
 __all__ = ["create_llms_txt_router", "generate_llms_txt"]
-__version__ = "0.5.4"
+__version__ = "0.5.5"
